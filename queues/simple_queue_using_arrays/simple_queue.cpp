11de4ea6843529fd8883796a7a30066df19dabbf
kalet Chavez <kalet.svdev@gmail.com>
2024-12-18 04:03:24 -0600

    :sparkles: learn about queue using linked list

--- queues/simple_queue_using_arrays/simple_queue.cpp
+++ queues/simple_queue_using_arrays/simple_queue.cpp
@@ -6,15 +6,60 @@ struct Queue {
     int front, rear, capacity;
     int* queue;
 
-    Queue( int size ): front(0), rear(-1), capacity(size), queue( new int[size]){};
+    Queue( int size ): front(0), rear(-1), capacity( size ), queue( new int[size] ){}
 
     ~Queue(){ delete[] queue; }
-
-    void queueEnqueue( int data ){
-        if( rear == capacity -1 ){
-            cout << "\nQueue is full\n";
-            return;
-        }
-        queue[++rear] = data;
-    }
 };
+
+bool isFull( const Queue& queue){
+    return ( queue.rear - queue.front + 1) == queue.capacity;
+}
+
+bool isEmpty( const Queue& queue ){
+    return queue.front > queue.rear;
+}
+
+void enqueue( Queue& queue, int value ){
+    if( isFull(queue)){
+        cout << "is full\n";
+        return ;
+    }
+    queue.queue[++queue.rear] = value;
+}
+
+int dequeue( Queue& queue ){
+    if( isEmpty( queue) ){
+        cout << "is empty\n";
+        return -1;
+    }
+    int value = queue.queue[queue.front];
+    queue.front++;
+    return value;
+}
+
+void showQueue( Queue& queue ){
+    for( int i = queue.front; i <= queue.rear; i++ ){
+        cout << queue.queue[i] << " ";
+    }
+    cout << "\n";
+}
+
+
+int main(){
+
+    Queue queue = Queue(8);
+
+    enqueue( queue, 10);
+    enqueue( queue, 18);
+    enqueue( queue, 23);
+    enqueue( queue, 4);
+    enqueue( queue, 2);
+
+    showQueue( queue);
+
+    dequeue( queue );
+
+    showQueue( queue );
+
+}
+
