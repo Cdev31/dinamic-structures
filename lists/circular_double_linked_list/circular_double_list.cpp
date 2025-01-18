8e2420e74f376fdff9a74c8fb6dda09c395aae73
kalet Chavez <kalet.svdev@gmail.com>
2025-01-18 16:41:05 -0600

    :sparkles: work insert methods in circular double linked list

--- lists/circular_double_linked_list/circular_double_list.cpp
+++ lists/circular_double_linked_list/circular_double_list.cpp
@@ -26,11 +26,48 @@ class CircularDoubleLinkedList{
             head->previus = head;
             return;
         }
+
         newNode->next = head;
         head->previus = newNode;
         head = newNode;
         head->previus = tail;
         tail->next = head;
+    }
+
+    void insertAtEnd( int data ){
+
+        Node* newNode = new Node( data );
+        
+        if( head == nullptr){
+            head = tail = newNode;
+            head->next = head;
+            head->previus = head;
+            return;
+        }
+
+        tail->next = newNode;
+        newNode->previus = tail;
+        tail = newNode;
+        tail->next = head;
+        head->previus = tail;
+    }
+
+    void insertAtPosition( int data, int position ){
+
+        Node* currentNode = head;
+        Node* newNode = new Node( data );
+
+        int counter = 1;
+        while( currentNode->next != head ){
+            if( counter == position) break;;
+            currentNode = currentNode->next;
+            counter++;
+        }
+
+        newNode->next = currentNode;
+        newNode->previus = currentNode->previus;
+        currentNode->previus->next = newNode;
+        currentNode->previus = newNode;
     }
 
     void printAsc(){
@@ -60,6 +97,15 @@ int main(int argc, char const *argv[])
 {
     CircularDoubleLinkedList firstList = CircularDoubleLinkedList();
 
+    //Insertar al final
+
+    firstList.insertAtEnd(9);
+    firstList.insertAtEnd(11);
+    firstList.insertAtEnd(12);
+
+    firstList.printAsc();
+
+    //Insertar al inicio
     firstList.insertAtBeginnig(2);
     firstList.insertAtBeginnig(3);
     firstList.insertAtBeginnig(8);
@@ -67,6 +113,13 @@ int main(int argc, char const *argv[])
     firstList.insertAtBeginnig(9);
     firstList.insertAtBeginnig(6);
     
+    firstList.printAsc();
+
+    //Insertar posicion
+
+    firstList.insertAtPosition( 13, 4);
+    firstList.insertAtPosition( 14, 4);
+
     firstList.printAsc();
     firstList.printDesc();
 
