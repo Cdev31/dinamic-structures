a71f1a466bdf78dbbd0fbcff674f21e25a46cb12
kalet Chavez <kalet.svdev@gmail.com>
2025-01-22 18:33:09 -0600

    :sparkles: work in circular queue using linked list

--- queues/circular_queue/using_linked_list/circular_queue.cpp
+++ queues/circular_queue/using_linked_list/circular_queue.cpp
@@ -14,7 +14,9 @@ class CircularQueue{
     Node* tail = nullptr;
 
     public:
-    bool isEmpty(){}
+    bool isEmpty(){
+        return head == nullptr;
+    }
 
     void enqueue( int data ){
         
@@ -30,10 +32,65 @@ class CircularQueue{
 
     }
 
-    void dequeue(){}
+    void dequeue(){
 
-    void peek(){}
+        if( isEmpty() ){
+            cout << "Error: Queue is empty" << endl;
+            return;
+        }
 
+        Node* currentNode = head;
 
+        while( currentNode->next != head ){
+            currentNode= currentNode->next;
+            if( currentNode->next == tail ) break;
+        }
+        Node* temporalNode = tail;
+
+        currentNode->next = head;
+        tail = currentNode;
+
+        delete temporalNode;
+
+    }
+
+    void peek(){
+        cout << head->data << endl;
+    }
+
+    void printQueue(){
+        Node* currentNode = head;
+
+        while( currentNode->next != head ){
+            cout << currentNode->data << " ";
+            currentNode = currentNode->next;
+        }
+        cout << currentNode->data << endl;
+    }
 
 };
+
+int main(int argc, char const *argv[])
+{
+    CircularQueue queue = CircularQueue();
+
+    cout << "Enqueue data" << endl;
+    queue.enqueue(2);
+    queue.enqueue(4);
+    queue.enqueue(3);
+    queue.enqueue(5);
+    queue.enqueue(9);
+    queue.enqueue(6);
+    queue.enqueue(7);
+
+    queue.printQueue();
+
+    cout << "Deque data" << endl;
+
+    queue.dequeue();
+    queue.dequeue();
+
+    queue.printQueue();
+
+    return 0;
+}
